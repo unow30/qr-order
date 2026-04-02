60e543bc8180ea9f09e86fbdfb2474759a1f8511
unow30 <yunho7205@gmail.com>
2026-04-02 15:53:36 +0900

    docker deploy setting test

--- apps/server/src/config/database.config.ts
+++ apps/server/src/config/database.config.ts
@@ -2,18 +2,23 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmQueryLogger } from '@server/common/logging/collect/typeorm-query-logger';
 
-export const getDatabaseConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
-  type: 'postgres',
-  host: configService.get<string>('DATABASE_HOST', 'localhost'),
-  port: configService.get<number>('DATABASE_PORT', 5432),
-  username: configService.get<string>('DATABASE_USER', 'qrorder'),
-  password: configService.get<string>('DATABASE_PASSWORD', 'qrorder_password'),
-  database: configService.get<string>('DATABASE_NAME', 'qrorder_db'),
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize:
-    configService.get<string>('NODE_ENV') !== 'production' ||
-    configService.get<string>('TYPEORM_SYNC') === 'true',
-  logger: new TypeOrmQueryLogger(),
-  maxQueryExecutionTime: parseInt(configService.get<string>('LOG_SLOW_QUERY_MS', '2000'), 10),
-  autoLoadEntities: true,
-});
+export const getDatabaseConfig = (configService: ConfigService): TypeOrmModuleOptions => {
+  const sslEnabled = configService.get<string>('DATABASE_SSL', 'false') === 'true';
+
+  return {
+    type: 'postgres',
+    host: configService.get<string>('DATABASE_HOST', 'localhost'),
+    port: configService.get<number>('DATABASE_PORT', 5432),
+    username: configService.get<string>('DATABASE_USER', 'qrorder'),
+    password: configService.get<string>('DATABASE_PASSWORD', 'qrorder_password'),
+    database: configService.get<string>('DATABASE_NAME', 'qrorder_db'),
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize:
+      configService.get<string>('NODE_ENV') !== 'production' ||
+      configService.get<string>('TYPEORM_SYNC') === 'true',
+    ssl: sslEnabled ? { rejectUnauthorized: false } : false,
+    logger: new TypeOrmQueryLogger(),
+    maxQueryExecutionTime: parseInt(configService.get<string>('LOG_SLOW_QUERY_MS', '2000'), 10),
+    autoLoadEntities: true,
+  };
+};
