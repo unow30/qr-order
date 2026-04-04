3a0a337a421d06fd699dcfae6ca7d5030dd207d2
unow30 <yunho7205@gmail.com>
2026-04-04 19:44:36 +0900

    set ec2:docker port

--- apps/server/src/config/redis.config.ts
+++ apps/server/src/config/redis.config.ts
@@ -5,10 +5,14 @@ let redisClient: Redis | null = null;
 
 export const getRedisClient = (configService: ConfigService): Redis => {
   if (!redisClient) {
+    const redisHost = configService.get<string>('REDIS_HOST', 'localhost');
+    const useTls = configService.get<string>('REDIS_TLS', 'false') === 'true';
+
     redisClient = new Redis({
-      host: configService.get<string>('REDIS_HOST', 'localhost'),
+      host: redisHost,
       port: configService.get<number>('REDIS_PORT', 6380),
       password: configService.get<string>('REDIS_PASSWORD'),
+      tls: useTls ? {} : undefined,
       retryStrategy: (times) => Math.min(times * 50, 2000),
     });
   }
