b9b3b6c18325950153b30736f03e1ae9aed5e360
unow30 <yunho7205@gmail.com>
2026-04-08 15:18:57 +0900

    버튼 레이아웃 수정

--- apps/web/src/pages/MenuDetailPage.tsx
+++ apps/web/src/pages/MenuDetailPage.tsx
@@ -202,7 +202,7 @@ export default function MenuDetailPage() {
         </div>
       </div>
 
-      <div className="fixed bottom-4 left-0 right-0 md:right-auto md:w-[480px] flex flex-col items-center px-[5%] md:px-5">
+      <div className="fixed bottom-4 left-0 right-0 md:right-auto md:w-[480px] flex flex-col gap-2 px-4">
         <button
           onClick={handleAddToCart}
           disabled={loading}
